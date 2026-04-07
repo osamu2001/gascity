8c7bd2193cebb1e4aba92b19112c7532ab12203e
julianknutsen <julianknutsen@users.noreply.github.com>
2026-04-07 23:54:58 +0200

    Expand provider and supervisor search paths

--- internal/api/handler_provider_readiness.go
+++ internal/api/handler_provider_readiness.go
@@ -520,6 +520,10 @@ func providerProbeSearchDirs(homeDir, goos, basePath string) []string {
 	return searchpath.Expand(homeDir, goos, basePath)
 }
 
+func dedupeProbeSearchDirs(dirs []string) []string {
+	return searchpath.Dedupe(dirs)
+}
+
 func providerProbeSearchPath(homeDir string) string {
 	return searchpath.ExpandPath(homeDir, providerProbeGOOS, providerProbePathEnv)
 }
